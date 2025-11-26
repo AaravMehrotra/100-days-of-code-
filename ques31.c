0601bdab5223c62b3971782f2f0cfb17ff81b338
Aarav Mehrotra <aaravmehrotra06@gmail.com>
2025-11-26 11:32:49 +0530

    Update ques31.c

--- ques31.c
+++ ques31.c
@@ -1,25 +1,20 @@
-// Write a program to take a number as input and print its equivalent binary representation.
-
 #include <stdio.h>
-
-int main() {
+int main() 
+{
     int num, i;
-    int binary[32];  // Array to store binary representation
+    int binary[32]; 
     printf("Enter a number: ");
     scanf("%d",&num);
 
-    // Convert to binary
     for (i = 0; num > 0; i++) {
         binary[i] = num % 2;
         num = num / 2;
     }
-
-    // Print binary representation
     printf("Binary representation: ");
     for (i = i - 1; i >= 0; i--) {
         printf("%d", binary[i]);
     }
     printf("\n");
-
     return 0;
 }
+
